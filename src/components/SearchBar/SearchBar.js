1f40dcf071737f318b013b81d0d3c3a80042fe2b
mp-web3 <mp.web3@gmail.com>
2023-05-17 09:25:56 +0200

    button removed from SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,19 +1,7 @@
 import React, { useState } from 'react';
 import './SearchBar.css';
-import Button from '../Button';
+/* import Button from '../Button'; */
 
-/*
-class SearchBar extends React.Component {
-  render() {
-    return (
-      <div className='SearchBar'>
-        <input placeholder='Enter a Song, Album, or Artist' />
-        <Button text='SEARCHING'/>
-      </div>
-    );
-  }
-}
-*/
 
 function SearchBar(props) {
   const [term, setTerm] = useState('')
